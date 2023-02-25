f90f1fe2a5fbe2d55b4079d71de924016365e533
Bruno Messias <bs.bruno.messias@gmail.com>
2023-02-25 10:52:36 -0300

    added firebase authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,14 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
 
 export const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user)
     }
 
     return (
         <div>
-            <h1>Sign in Page</h1>
+            <button onClick={logGoogleUser}>Sign in Page</button>
         </div>
     )
 }
