08d9d67b1e5869dfe848c7a8136a771889e00176
Bruno Messias <bs.bruno.messias@gmail.com>
2023-02-27 14:37:07 -0300

    form almost done

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,17 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { SignUpForm } from "../../components/sign-up-form/sign-up-form.component";
 
 export const SignIn = () => {
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return (
         <div>
-            <button onClick={logGoogleUser}>Sign in Page</button>
+            <button onClick={logGoogleUser}>Sign in Page With Google</button>
+            <SignUpForm />
         </div>
     )
 }
